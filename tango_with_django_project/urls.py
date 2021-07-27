ba93fdc2a5a55e0fc15e277a087debb35129eb10
Yue Zhong <47201027+zhongyuesz@users.noreply.github.com>
2021-07-27 21:54:35 +0100

    chap3_url mapping

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rango import views
+from django.urls import include
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('rango/', include('rango.urls')),
 ]
