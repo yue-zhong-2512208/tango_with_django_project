73bbbe571069bd00dad8a467ceecd0fa1291d3cb
Yue Zhong <47201027+zhongyuesz@users.noreply.github.com>
2021-07-27 22:31:19 +0100

    chap4_first template used

--- rango/views.py
+++ rango/views.py
@@ -6,3 +6,8 @@ def index(request):
 
 def about(request):
     return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+
+
+def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
