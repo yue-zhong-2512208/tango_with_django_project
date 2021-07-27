585187d561f1e3ff472309f1fcdc628f6fde1366
Yue Zhong <47201027+zhongyuesz@users.noreply.github.com>
2021-07-27 23:12:35 +0100

    chap4_media served

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from rango import views
 from django.urls import include
+from django.conf import settings 
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('rango/', include('rango.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
