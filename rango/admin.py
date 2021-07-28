e79ef28c23b83e579b4b2347d634a2207aba4176
Yue Zhong <47201027+zhongyuesz@users.noreply.github.com>
2021-07-28 22:10:43 +0100

    chap6_category template created, first step of creating a details page

--- rango/admin.py
+++ rango/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
-
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
-
 admin.site.register(Page, PageAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
