57d9ba38a9996b9de1afe9091ed300c3b7459f55
Yue Zhong <47201027+zhongyuesz@users.noreply.github.com>
2021-07-28 16:46:11 +0100

    chap5_population script created, fixed bug of showing Page object (*) instead of the title

--- rango/models.py
+++ rango/models.py
@@ -2,14 +2,12 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+
     class Meta:
         verbose_name_plural = 'Categories'
+    # __str__() = toString()
     def __str__(self):
         return self.name
-
-# __str__() = toString()
-def __str__(self):
-    return self.name
 
 class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE) 
@@ -17,7 +15,7 @@ class Page(models.Model):
     url = models.URLField() 
     views = models.IntegerField(default=0)
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
 
