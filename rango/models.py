dd7458d0e46642d6cbd86f77bac56c0f1e7ff7e7
Yue Zhong <47201027+zhongyuesz@users.noreply.github.com>
2021-07-28 16:05:45 +0100

    chap5_database created & migrated, the admin interface configured

--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    class Meta:
+        verbose_name_plural = 'Categories'
+    def __str__(self):
+        return self.name
 
 # __str__() = toString()
 def __str__(self):
@@ -15,3 +19,5 @@ class Page(models.Model):
 
 def __str__(self):
     return self.title
+
+
