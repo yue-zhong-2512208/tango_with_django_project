dc2064aeb58632b571c67254cf6275a83e4c2cfc
Yue Zhong <47201027+zhongyuesz@users.noreply.github.com>
2021-07-28 23:59:40 +0100

    chap6_test OK
    Add initialized value of views to pages; changed the formats of the link to index in category.html

--- populate_rango.py
+++ populate_rango.py
@@ -9,25 +9,25 @@ from rango.models import Category, Page
 def populate():
     python_pages = [
         {'title': 'Official Python Tutorial',
-        'url': 'http://docs.python.org/3/tutorial/'},
+        'url': 'http://docs.python.org/3/tutorial/', 'views': '45',},
         {'title': 'How to Think like a Computer Scientist',
-        'url': 'http://www.greenteapress.com/thinkpython/'},
+         'url': 'http://www.greenteapress.com/thinkpython/', 'views': '63', },
         {'title': 'Learn Python in 10 Minutes',
-        'url': 'http://www.korokithakis.net/tutorials/python/'}]
+         'url': 'http://www.korokithakis.net/tutorials/python/', 'views': '20', }]
 
     django_pages = [
         {'title': 'Official Django Tutorial',
-        'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'}, 
+         'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', 'views': '23', },
         {'title': 'Django Rocks',
-        'url': 'http://www.djangorocks.com/'}, 
+         'url': 'http://www.djangorocks.com/', 'views': '27', },
         {'title': 'How to Tango with Django',
-        'url': 'http://www.tangowithdjango.com/'}]
+         'url': 'http://www.tangowithdjango.com/', 'views': '14', }]
 
     other_pages = [
         {'title': 'Bottle',
-        'url': 'http://bottlepy.org/docs/dev/'}, 
+         'url': 'http://bottlepy.org/docs/dev/', 'views': '19', },
         {'title': 'Flask',
-        'url': 'http://flask.pocoo.org'}]
+         'url': 'http://flask.pocoo.org', 'views': '13', }]
         
     cats = {
         'Python': {'pages': python_pages, 'views': 128, 'likes' : 64}, 
