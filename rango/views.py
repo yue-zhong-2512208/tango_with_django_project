39f6f20e46ca463d627c789ea58e132fa137d7aa
Yue Zhong <47201027+zhongyuesz@users.noreply.github.com>
2021-07-28 21:06:56 +0100

    chap6_first data-driven webpage done

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,4 @@
+from rango.models import Category
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -7,3 +8,11 @@ def index(request):
 
 def about(request):
     return render(request, 'rango/about.html')
+
+
+def index(request):
+    category_list = Category.objects.order_by('-likes')[:5] #without '-' will from the most to the least
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    return render(request, 'rango/index.html', context=context_dict)
