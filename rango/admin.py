95f1cc5047fb72b5a4eefaf1dd127e77f6f3b24b
Yue Zhong <47201027+zhongyuesz@users.noreply.github.com>
2021-07-29 23:45:18 +0100

    chap9_additional user attributes added

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
@@ -10,3 +11,5 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(UserProfile)
