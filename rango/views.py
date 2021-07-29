bed959aba94ecfe6469870556b231c730cbcb5d9
Yue Zhong <47201027+zhongyuesz@users.noreply.github.com>
2021-07-29 19:05:03 +0100

    chap7_ModelForm Class created; Add Category View created

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,8 @@ from rango.models import Category
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Page
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
 
 def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
@@ -32,3 +34,15 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
         context_dict['pages'] = None
     return render(request, 'rango/category.html', context=context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
