fcd0fb4fa4fde79967534d17f9acd23cff25c3a2
Yue Zhong <47201027+zhongyuesz@users.noreply.github.com>
2021-07-29 23:31:26 +0100

    chap9_password validators added

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -90,16 +90,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', 'OPTIONS': {'min_length': 6,}
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', 'OPTIONS': {'min_length': 6, }
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', 'OPTIONS': {'min_length': 6,}
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', 'OPTIONS': {'min_length': 6, }
     },
 ]
 
