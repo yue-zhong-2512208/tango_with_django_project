0b4f98fbfc9198f1d650a8aeb9094973562058fc
Yue Zhong <47201027+zhongyuesz@users.noreply.github.com>
2021-07-29 23:24:29 +0100

    chap9_password hashing customized

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -103,6 +103,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
