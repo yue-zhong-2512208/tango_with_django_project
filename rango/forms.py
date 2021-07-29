b771fa4f6f6c6048ee5ea20a66f9db1b72e3d6b3
Yue Zhong <47201027+zhongyuesz@users.noreply.github.com>
2021-07-29 19:47:03 +0100

    chap7_clean function added

--- rango/forms.py
+++ rango/forms.py
@@ -19,3 +19,11 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         exclude = ('category',)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        return cleaned_data
