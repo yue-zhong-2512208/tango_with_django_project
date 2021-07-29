a0f93ac4d85d8c67fb3bad76d93e031e346010e1
Yue Zhong <47201027+zhongyuesz@users.noreply.github.com>
2021-07-29 20:42:21 +0100

    chap7_excercises attempted, tried to add a page

--- rango/forms.py
+++ rango/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from rango.models import Page, Category
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="Please enter the catergory name.")
+    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH,
+                           help_text="Please enter the catergory name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -12,7 +13,8 @@ class CategoryForm(forms.ModelForm):
         fields = ('name',)
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
+    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH,
+                            help_text="Please enter the title of the page.")
     url = forms.URLField(max_length=200, help_text="Please enter the URL of the pages.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
