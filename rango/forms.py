d8a896079afd481d732b76fe03cc356fc57c8265
Yue Zhong <47201027+zhongyuesz@users.noreply.github.com>
2021-07-29 19:41:56 +0100

    chap7_category view added; index.html modified; demo attempted

--- rango/forms.py
+++ rango/forms.py
@@ -11,11 +11,11 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = ('name',)
 
-class Pageform(forms.ModelForm):
-    title = forms.CharField(max_length=128,help_text="Please enter the title of the page.")
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
     url = forms.URLField(max_length=200, help_text="Please enter the URL of the pages.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
         model = Page
-        exclude = ('category')
+        exclude = ('category',)
