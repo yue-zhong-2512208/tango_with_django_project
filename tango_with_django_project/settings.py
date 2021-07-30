cafdd686f2d427e15357a18c13c5d47e1afb7985
Yue Zhong <47201027+zhongyuesz@users.noreply.github.com>
2021-07-30 04:12:59 +0100

    chap9_test OK
    removed hasher password from settings.py;
    added link back to homepage if registered;

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -90,25 +90,18 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', 'OPTIONS': {'min_length': 6, }
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', 
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', 'OPTIONS': {'min_length': 6, }
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', 
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', 'OPTIONS': {'min_length': 6, }
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', 
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', 'OPTIONS': {'min_length': 6, }
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', 
     },
 ]
-
-PASSWORD_HASHERS = (
-    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-    'django.contrib.auth.hashers.BCryptPasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-)
 
 LOGIN_URL = 'rango:login'
 
